3ada27394dbfec700fea7f4307d351af6b00efef
hecool108 <hecool108@gmail.com>
2024-08-17 18:48:12 +0800

    feat: use preset query

--- src/schema-fetcher.ts
+++ src/schema-fetcher.ts
@@ -1,94 +1,95 @@
 import { createClient } from 'graphqurl';
 import { gql } from 'graphql-request';
+import { getIntrospectionQuery } from 'graphql';
 const { HASURA_END_POINT, HASURA_ADMIN_KEY } = process.env;
 
-const GetSchema = gql`
-  query IntrospectionQuery {
-    __schema {
-      queryType {
-        name
-      }
-      mutationType {
-        name
-      }
-      subscriptionType {
-        name
-      }
-      types {
-        ...FullType
-      }
-      directives {
-        name
-        description
-        args {
-          ...InputValue
-        }
-        onOperation #Often needs to be deleted to run query
-        onFragment #Often needs to be deleted to run query
-        onField #Often needs to be deleted to run query
-      }
-    }
-  }
+// const GetSchema = gql`
+//   query IntrospectionQuery {
+//     __schema {
+//       queryType {
+//         name
+//       }
+//       mutationType {
+//         name
+//       }
+//       subscriptionType {
+//         name
+//       }
+//       types {
+//         ...FullType
+//       }
+//       directives {
+//         name
+//         description
+//         args {
+//           ...InputValue
+//         }
+//         onOperation #Often needs to be deleted to run query
+//         onFragment #Often needs to be deleted to run query
+//         onField #Often needs to be deleted to run query
+//       }
+//     }
+//   }
 
-  fragment FullType on __Type {
-    kind
-    name
-    description
-    fields(includeDeprecated: true) {
-      name
-      description
-      args {
-        ...InputValue
-      }
-      type {
-        ...TypeRef
-      }
-      isDeprecated
-      deprecationReason
-    }
-    inputFields {
-      ...InputValue
-    }
-    interfaces {
-      ...TypeRef
-    }
-    enumValues(includeDeprecated: true) {
-      name
-      description
-      isDeprecated
-      deprecationReason
-    }
-    possibleTypes {
-      ...TypeRef
-    }
-  }
+//   fragment FullType on __Type {
+//     kind
+//     name
+//     description
+//     fields(includeDeprecated: true) {
+//       name
+//       description
+//       args {
+//         ...InputValue
+//       }
+//       type {
+//         ...TypeRef
+//       }
+//       isDeprecated
+//       deprecationReason
+//     }
+//     inputFields {
+//       ...InputValue
+//     }
+//     interfaces {
+//       ...TypeRef
+//     }
+//     enumValues(includeDeprecated: true) {
+//       name
+//       description
+//       isDeprecated
+//       deprecationReason
+//     }
+//     possibleTypes {
+//       ...TypeRef
+//     }
+//   }
 
-  fragment InputValue on __InputValue {
-    name
-    description
-    type {
-      ...TypeRef
-    }
-    defaultValue
-  }
+//   fragment InputValue on __InputValue {
+//     name
+//     description
+//     type {
+//       ...TypeRef
+//     }
+//     defaultValue
+//   }
 
-  fragment TypeRef on __Type {
-    kind
-    name
-    ofType {
-      kind
-      name
-      ofType {
-        kind
-        name
-        ofType {
-          kind
-          name
-        }
-      }
-    }
-  }
-`;
+//   fragment TypeRef on __Type {
+//     kind
+//     name
+//     ofType {
+//       kind
+//       name
+//       ofType {
+//         kind
+//         name
+//         ofType {
+//           kind
+//           name
+//         }
+//       }
+//     }
+//   }
+// `;
 
 const client = createClient({
   endpoint: HASURA_END_POINT!,
@@ -97,6 +98,6 @@ const client = createClient({
   },
 });
 export const fetchSchema = async () => {
-  const result = await client.query({query:GetSchema});
+  const result = await client.query({query:getIntrospectionQuery.toString()});
   console.log(result);
 };
