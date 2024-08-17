d9ab73900c4cbe7ecec32ad6e9795adbc96d6534
hecool108 <hecool108@gmail.com>
2024-08-17 18:42:13 +0800

    feat: use graphqurl

--- src/schema-fetcher.ts
+++ src/schema-fetcher.ts
@@ -1,9 +1,102 @@
+import { createClient } from 'graphqurl';
+import { gql } from 'graphql-request';
+const { HASURA_END_POINT, HASURA_ADMIN_KEY } = process.env;
 
+const GetSchema = gql`
+  query IntrospectionQuery {
+    __schema {
+      queryType {
+        name
+      }
+      mutationType {
+        name
+      }
+      subscriptionType {
+        name
+      }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        args {
+          ...InputValue
+        }
+        onOperation #Often needs to be deleted to run query
+        onFragment #Often needs to be deleted to run query
+        onField #Often needs to be deleted to run query
+      }
+    }
+  }
 
-import { exec } from 'child_process';
-import { promisify } from 'util';
-const {HASURA_END_POINT,HASURA_ADMIN_KEY} = process.env 
-const execAsync = promisify(exec);
-export const fetchSchema = async ()=>{
-  await execAsync(`gq ${HASURA_END_POINT} -H 'X-Hasura-Admin-Secret: ${HASURA_ADMIN_KEY}' --introspect > src/schema.graphql`);
-}
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    description
+    type {
+      ...TypeRef
+    }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+        }
+      }
+    }
+  }
+`;
+
+const client = createClient({
+  endpoint: HASURA_END_POINT!,
+  headers: {
+    'X-Hasura-Admin-Secret': HASURA_ADMIN_KEY!,
+  },
+});
+export const fetchSchema = async () => {
+  const result = await client.query({query:GetSchema});
+  console.log(result);
+};
